11a9288f57e33c386906139568e7236f978befb8
NiAlvarez <nicofacu2014@gmail.com>
2021-05-24 23:38:17 -0300

    first commit, initial react setup

---
+++ src/Components/UI/LogIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogIn = () => {
+    return (
+        <div className="mx-auto p-5">
+           <span>Holis</span> 
+        </div>
+    )
+}
+
+export default LogIn
